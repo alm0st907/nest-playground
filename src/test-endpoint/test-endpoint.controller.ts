fd2a303986725336b1cdfba774be5e34c1b35cf2
Garrett Rudisill <garrett.rudisill@gmail.com>
2023-06-11 20:19:29 -0700

    playing around with accessing the request body and posting through swagger/postman

--- src/test-endpoint/test-endpoint.controller.ts
+++ src/test-endpoint/test-endpoint.controller.ts
@@ -1,5 +1,5 @@
-import {  Controller, Get, Post } from "@nestjs/common";
-import {  ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 @Controller('test-endpoint')
 @ApiTags('test-endpoint')
@@ -9,5 +9,19 @@ export class TestEndpointController {
     return (
       'This is the test endpoint + the datetime: ' + new Date().toLocaleString()
     );
+  }
+
+  @Post(`just-the-body`)
+  @ApiBody({ type: String })
+  postTestEndpoint(@Body() req: any): string {
+    return `this is the ${JSON.stringify(req)}`;
+  }
+
+  @Post('the-whole-req')
+  @ApiBody({ type: String })
+  postTestEndpointBetter(@Req() req: any): string {
+    return `this is the better way to get the request: ${JSON.stringify(
+      req.body,
+    )}`;
   }
 }
